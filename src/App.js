28d0d01720d649cfa0e0066cbb30e5aedb672f8a
Stephen Pope <stephengpope@gmail.com>
2019-10-12 11:57:50 -0700

    updates to app, gaining some traction

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 
 import logo from './logo.svg';
 import './App.css';
+
+import Today from './components/Today'
 import Greetings from './components/Greetings'
 import Welcome from './components/Welcome'
 
 function App() {
   return (
     <div className="App">
-      <Greetings />
-      <Welcome />
+      <div class="NavBar">
+        <a href="/">Today</a> | <a href="/">Focus</a> | <a href="/">Set and Plan Goals</a>
+      </div>
+
+      <Today />
     </div>
   );
 }
---
+++ src/components/Today.js
@@ -0,0 +1,13 @@
+import React, { component } from 'react'
+
+const Today = () => {
+
+    return <div class="Today">
+        <h1>Today</h1>
+
+        <h2>Todo:</h2>
+        <h2>Completed:</h2>
+    </div>;
+}
+
+export default Today;
