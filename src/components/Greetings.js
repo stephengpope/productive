bd68d1b53cf987758947561798a91f787a2ae61e
Stephen Pope <stephengpope@gmail.com>
2019-10-12 09:32:39 -0700

    new files

---
+++ src/components/Greetings.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+
+const Greetings = () => <h1>Hello</h1>
+
+export default Greetings
---
+++ src/components/Welcome.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+
+class Welcome extends Component {
+
+
+  render() {
+
+    return <h1>Hello, You're Welcome!</h1>;
+  }
+}
+
+export default Welcome
